3ec5935823cfcf6a882d6cfe6df42164bef94de1
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-04 18:54:43 +0530

    movie row created

---
+++ src/components/Row.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Movie from './Movie';
+
+const Row = ({ title, fetchURL }) => {
+
+    const [movies, setMovies] = useState([]);
+
+
+    useEffect(() => {
+
+        axios.get(fetchURL).then((response) => {
+            setMovies(response.data.results);
+        })
+    }, [fetchURL])
+
+    console.log(movies);
+
+    return (
+        <>
+            <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+            <div className="realtive flex items-center">
+                <div id={'slider'} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+                    {
+                        movies.map((item, id) => (
+                            <Movie item={item} key={id} />
+                        ))
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Row
