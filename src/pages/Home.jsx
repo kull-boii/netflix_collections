ec146f53fb52c3f9ec2039e4230824b3525c878d
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-06 21:36:33 +0530

    added auth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
             <Row rowID='1' title="Upcoming" fetchURL={requests.requestUpcoming} />
             <Row rowID='2' title="Popular" fetchURL={requests.requestPopular} />
             <Row rowID='3' title="Trending" fetchURL={requests.requestTrending} />
-            <Row rowID='3' title="Top Rated" fetchURL={requests.requestTopRated} />
-            <Row rowID='4' title="Horror" fetchURL={requests.requestHorror} />
+            <Row rowID='4' title="Top Rated" fetchURL={requests.requestTopRated} />
+            <Row rowID='5' title="Horror" fetchURL={requests.requestHorror} />
         </>
     )
 }
